9886636061be510e64b3043442a4624d7180c7a9
Eduardo Guzman <eduardo.guzman@innovamics.com>
2025-02-07 09:51:10 +0100

    Ma version 1 marche

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 int* allouerTableauEntiersV1(int const taille)
 {
   int *t = malloc(sizeof(int)*taille);
+  return t;
 }
 
 void afficherTab(int* tab, int const taille)
@@ -24,5 +25,13 @@ int main()
   int taille = 10;
   int* tab = NULL;
 
+  // test de la version 1
+  tab =  allouerTableauEntiersV1(taille);
+  afficherTab(tab, taille);
+  free(tab);
+  tab = NULL;
+
+
+  return 0;
 
 }
