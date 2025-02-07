4bd087479162538cbe2a326bd3912ec7135c96c9
Eduardo Guzman <eduardo.guzman@innovamics.com>
2025-02-07 09:46:06 +0100

    Ajout de la fonction afficher

--- main.c
+++ main.c
@@ -1,1 +1,28 @@
-int main(){}
+#include<stdio.h>
+#include<stdlib.h>
+
+int* allouerTableauEntiersV1(int const taille)
+{
+  int *t = malloc(sizeof(int)*taille);
+}
+
+void afficherTab(int* tab, int const taille)
+{
+  if (tab!= NULL)
+  {
+    for (int i = 0; i < taille; ++i)
+    {
+      printf("%d ", tab[i]);
+    }
+    printf("\n");
+  }
+}
+
+
+int main()
+{
+  int taille = 10;
+  int* tab = NULL;
+
+
+}
